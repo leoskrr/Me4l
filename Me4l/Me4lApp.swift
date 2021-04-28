e7c384c02bf7919dd56db2737592b45ebc7aa24f
Leonardo Viana <sonleo2013@gmail.com>
2021-04-27 20:36:42 -0400

    chore: reset project

--- Me4l/Me4lApp.swift
+++ Me4l/Me4lApp.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 @main
 struct Me4lApp: App {
-    let persistenceController = PersistenceController.shared
+//    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
