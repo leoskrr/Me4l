08d1b203414f1093e79d2db009acaf12ec189174
Leonardo Viana <sonleo2013@gmail.com>
2021-04-27 20:24:09 -0400

    Initial Commit

---
+++ Me4l/Me4lApp.swift
@@ -0,0 +1,20 @@
+//
+//  Me4lApp.swift
+//  Me4l
+//
+//  Created by Leonardo Viana on 27/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Me4lApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
